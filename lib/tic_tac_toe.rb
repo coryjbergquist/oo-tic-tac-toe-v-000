e8c8b5e66f918e2092cb6916b4694a01fdf35453
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-15 00:15:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,10 @@ def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
 
-
+def valid_move?(board, index)
+    index.between?(0, 8) && !position_taken?(board, index) 
+ end
+ 
+ 
 
 end
